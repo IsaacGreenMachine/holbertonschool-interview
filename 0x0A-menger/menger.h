7066fda812390d31065b40cfaa64e136e2936fa5
Isaac Green <zuderg@gmail.com>
2022-09-10 13:38:34 -0500

    reverting

--- 0x0A-menger/menger.h
+++ 0x0A-menger/menger.h
@@ -1,9 +1,12 @@
-#ifndef _MENGER_SPONGE_H_
-#define _MENGER_SPONGE_H_
+#ifndef _MENGER_H_
+#define _MENGER_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 void menger(int level);
+void drawMenger(int level, int x, int y, int *mptr, int size);
 
 #endif
