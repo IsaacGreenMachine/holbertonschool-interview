36ccfe9fc3c0d53d13342087e2932250b48c312d
Isaac Green <zuderg@gmail.com>
2022-09-10 13:35:57 -0500

    trying to appease valgrind

--- 0x0A-menger/0-menger.c
+++ 0x0A-menger/0-menger.c
@@ -1,79 +1,35 @@
 #include "menger.h"
 /**
- * menger - draws a 2D Menger Sponge
- * @level: the level of the Menger Sponge to draw
- * Return: void
- * A level 0 sponge is represented by the # character
- * A level 1 sponge is a 3x3 square of level 0 sponges,
- *      except for the center one, which is left empty
- * A level 2 sponge is a 3x3 square of level 1 sponges,
- *      except for the center one, which is left empty
- * A level N sponge is a 3x3 square of level N-1 sponges,
- *      except for the center one, which is left empty
- * A level N Menger sponge has dimensions sized at 3^N
+ * selectChar - selects character to print for menger sponge
+ * @row: current row
+ * @column: current column
+ * Return: character to print
  */
-void menger(int level)
+char selectChar(int row, int column)
 {
-int i;
-int j;
-int meng[1000][1000];
-int *mptr = &meng[0][0];
-if (level < 0)
-return;
-else if (level == 0)
-{
-printf("#\n");
-return;
-}
-else
-{
-drawMenger(level, 0, 0, mptr, pow(3, level));
-for (i = 0; i < pow(3, level); i++)
-{
-for (j = 0; j < pow(3, level); j++)
-{
-if (meng[i][j] == 1)
-printf("#");
-else
-printf(" ");
-}
-printf("\n");
-
-}
-}
+	for (; row || column; row /= 3, column /= 3)
+	{
+		if (row % 3 == 1 && column % 3 == 1)
+			return (' ');
+	}
+	return ('#');
 }
 
 /**
- * drawMenger - draws a 2D Menger Sponge recursively
- * @level: the level of the Menger Sponge to draw
- * @x: x position to start drawing
- * @y: y position to start drawing
- * @mptr: pointer to array
- * @size: size of one dimension of menger sponge
- * Return: void
+ * menger - prints a menger sponge
+ * @level: level of menger to print
  */
-void drawMenger(int level, int x, int y, int *mptr, int size)
+void menger(int level)
 {
-int i;
-int j;
-int newx;
-int newy;
-if (level == 0)
-*(mptr + (x) + (1000 * y)) = 1;
-else
-{
-for (i = 0; i < 3; i++)
-{
-for (j = 0; j < 3; j++)
-{
-if (!(i == 1 && j == 1))
-{
-newx = (int) (pow(3, (level - 1)) * i) + x;
-newy = (int) (pow(3, (level - 1)) * j) + y;
-drawMenger(level - 1, newx, newy, mptr, size);
-}
-}
-}
-}
-}
+	int row, column, size;
 
+	if (level < 0)
+		return;
+	size = pow(3, level);
+	for (row = 0; row < size; row++)
+	{
+		for (column = 0; column < size; column++)
+			putchar(selectChar(row, column));
+		putchar('\n');
+	}
+}
--- 0x0A-menger/menger.h
+++ 0x0A-menger/menger.h
@@ -1,12 +1,9 @@
-#ifndef _MENGER_H_
-#define _MENGER_H_
-
+#ifndef _MENGER_SPONGE_H_
+#define _MENGER_SPONGE_H_
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
 
 void menger(int level);
-void drawMenger(int level, int x, int y, int *mptr, int size);
 
 #endif
