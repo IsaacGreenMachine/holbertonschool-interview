beff15e41b3b3bed9ee079d118e35351a3021b5b
Isaac Green <zuderg@gmail.com>
2022-11-15 15:45:51 -0600

    adding pycodestyled code

---
+++ 0x0F-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""module for makeChange function"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total.
+
+    Return: fewest number of coins needed to meet total
+            If total is 0 or less, return 0
+            If total cannot be met , return -1
+    total - int, number to be made by coins
+    coins - list of the values of the coins in your possession
+            The value of a coin will always be an integer greater than 0
+    you have an infinite number of each denomination of coin in the list
+    This is a (2n)log(n) solution.
+    """
+    if total <= 0:
+        return 0
+    count = 0
+    # nlog(n)
+    coins.sort(reverse=True)
+    # n
+    for coin in coins:
+        count += total // coin
+        total %= coin
+    return -1 if total else count
