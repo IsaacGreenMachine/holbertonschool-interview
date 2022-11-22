1734f304b71475f13836bf205f5cf80525d7fc6d
Isaac Green <zuderg@gmail.com>
2022-11-22 14:34:08 -0600

    adding task

---
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" module for island_perimeter function """
+
+
+def island_perimeter(grid):
+    '''
+    returns the perimeter of the island described in grid:
+        grid is a list of list of integers:
+            0 represents water
+            1 represents land
+        Each cell is square, with a side length of 1
+        Cells are connected horizontally/vertically (not diagonally).
+        grid is rectangular, with its width and height not exceeding 100
+        The grid is completely surrounded by water
+        There is only one island (or nothing).
+        The island doesnt have “lakes”
+        (water inside that isnt connected to the water surrounding the island).
+    '''
+    perimeter = 0
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if grid[row][column] == 1:
+                # only checking top and left sides of island since all islands
+                # have equal number of top and bottom, left and right sides
+                perimeter += [grid[row - 1][column],
+                              grid[row][column - 1],
+                              ].count(0)
+    return perimeter * 2
