e439c72e3ad03d88b3d9add8ed7592ba5e5f33ec
Isaac Green <zuderg@gmail.com>
2022-11-28 17:44:03 -0600

    rewriting in fewer lines

--- 0x15-island_perimeter/0-island_perimeter.py
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -17,19 +17,18 @@ def island_perimeter(grid):
         (water inside that isnt connected to the water surrounding the island).
     '''
     perimeter = 0
-    for row in range(len(grid)):
-        for column in range(len(grid[row])):
+    grid_l = len(grid)
+    if grid_l > 0:
+        grid_h = len(grid[0])
+    else:
+        grid_h = 0
+    perimeter += grid_l + grid_h
+    for row in range(1, grid_l):
+        for column in range(1, grid_h):
             if grid[row][column] == 1:
                 # only checking top and left sides of island since all islands
                 # have equal number of top and bottom, left and right sides
-                sides = []
-                if row == 0:
-                    sides.append(0)
-                else:
-                    sides.append(grid[row - 1][column])
-                if column == 0:
-                    sides.append(0)
-                else:
-                    sides.append(grid[row][column - 1])
-                perimeter += sides.count(0)
+                perimeter += [
+                    grid[row - 1][column],
+                    grid[row][column - 1]].count(0)
     return perimeter * 2
